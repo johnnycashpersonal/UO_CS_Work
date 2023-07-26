8d453ea866fabef0dcc125f0256624566deda962
john m <jmoore24@uoregon.edu>
2023-07-26 01:13:29 -0700

    second set of updates

--- CS211/OOPinPython/propertydecorators.py
+++ CS211/OOPinPython/propertydecorators.py
@@ -3,14 +3,36 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.'+ last + '@company.com' #this is an alternative way to do the same thing as above.
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+    
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
     
-emp_1 = Employee('Hunter', 'Schafer')
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+    
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+
+
+emp_1 = Employee('John', 'Schafer')
+
+emp_1.fullname = 'Hunter Schafer'
 
 print(emp_1.first)
 print(emp_1.email)
-print(emp_1.fullname())
+print(emp_1.fullname)
 
+del emp_1.fullname
+
+#this is a nice feature - we can access attributes without having getters and setters everywhere, but we can still use getters and setters if we want or need to.
